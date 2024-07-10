71bc3927659e4a05f16c6297818d94008702e788
MAHossain1 <mahpro110@gmail.com>
2024-07-10 14:11:07 +0600

    Implemented CRUD for academicDepartment.

---
+++ src/app/modules/academicDepartment/academicDepartment.controller.ts
@@ -0,0 +1,72 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { AcademicDepartmentServices } from './academicDepartment.service';
+
+const createAcademicDepartmentIntoDB = catchAsync(
+  async (req: Request, res: Response): Promise<void> => {
+    const result =
+      await AcademicDepartmentServices.createAcademicDepartmentIntoDB(req.body);
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: 'Academic Department created successfully',
+      data: result,
+    });
+  },
+);
+
+const getAllAcademicDepartment = catchAsync(
+  async (req: Request, res: Response): Promise<void> => {
+    const result = await AcademicDepartmentServices.getAllAcademicDepartment();
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: 'Academic faculties retrieved successfully',
+      data: result,
+    });
+  },
+);
+
+const getDepartmentByID = catchAsync(
+  async (req: Request, res: Response): Promise<void> => {
+    const { departmentId } = req.params;
+
+    const result =
+      await AcademicDepartmentServices.getDepartmentByID(departmentId);
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: 'Academic Department retrieved successfully',
+      data: result,
+    });
+  },
+);
+
+const updateAcademicDepartment = catchAsync(
+  async (req: Request, res: Response): Promise<void> => {
+    const { departmentId } = req.params;
+    const result = await AcademicDepartmentServices.updateAcademicDepartment(
+      departmentId,
+      req.body,
+    );
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: 'Academic Department updated successfully.',
+      data: result,
+    });
+  },
+);
+
+export const AcademicDepartmentControllers = {
+  createAcademicDepartmentIntoDB,
+  getAllAcademicDepartment,
+  getDepartmentByID,
+  updateAcademicDepartment,
+};
---
+++ src/app/modules/academicDepartment/academicDepartment.routes.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import validateRequest from '../../config/middlewares/validateRequest';
+import { AcademicDepartmentControllers } from './academicDepartment.controller';
+import { AcademicDepartmentValidationSchema } from './academicDepartment.validation';
+
+const router = express.Router();
+
+router.post(
+  '/create-academic-department',
+  validateRequest(
+    AcademicDepartmentValidationSchema.createAcademicDepartmentValidationSchema,
+  ),
+  AcademicDepartmentControllers.createAcademicDepartmentIntoDB,
+);
+
+router.get('/', AcademicDepartmentControllers.getAllAcademicDepartment);
+router.get('/:departmentId', AcademicDepartmentControllers.getDepartmentByID);
+router.patch(
+  '/:departmentId',
+  validateRequest(
+    AcademicDepartmentValidationSchema.updateAcademicDepartmentValidationSchema,
+  ),
+  AcademicDepartmentControllers.updateAcademicDepartment,
+);
+
+export const AcademicDepartmentRoutes = router;
