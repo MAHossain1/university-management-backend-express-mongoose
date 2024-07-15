eb19ac76b3ad1504f941557b85ad7cca16905e33
MAHossain1 <mahpro110@gmail.com>
2024-07-15 14:05:20 +0600

    Created offeredCourse interface, model, validationSchema.

---
+++ src/app/modules/offeredCourse/offeredCourse.model.ts
@@ -0,0 +1,69 @@
+import { model, Schema } from 'mongoose';
+import { TOfferedCourse } from './offeredCourse.interface';
+import { Days } from './offeredCourse.constant';
+
+const offeredCourseSchema = new Schema<TOfferedCourse>(
+  {
+    semesterRegistration: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'semesterRegistration',
+    },
+    academicSemester: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'academicSemester',
+    },
+    academicFaculty: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'academicFaculty',
+    },
+    academicDepartment: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'academicDepartment',
+    },
+    course: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'course',
+    },
+    faculty: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'faculty',
+    },
+    maxCapacity: {
+      type: Number,
+      required: true,
+    },
+    section: {
+      type: Number,
+      required: true,
+    },
+    days: {
+      type: String,
+      enum: {
+        values: Days,
+        message: `{VALUE} is not a valid gender.`,
+      },
+    },
+    startTime: {
+      type: String,
+      required: true,
+    },
+    endTime: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const OfferedCourse = model<TOfferedCourse>(
+  'OfferedCourse',
+  offeredCourseSchema,
+);
