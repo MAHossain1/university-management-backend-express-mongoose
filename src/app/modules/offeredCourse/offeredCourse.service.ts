73f0e58493997f22cd0f7a08462f70ad78312f8a
MAHossain1 <mahpro110@gmail.com>
2024-07-15 18:02:26 +0600

    Handled faculty time conflict when generating offeredCourses.

--- src/app/modules/offeredCourse/offeredCourse.service.ts
+++ src/app/modules/offeredCourse/offeredCourse.service.ts
@@ -16,6 +16,9 @@ const createOfferedCourse = async (payload: TOfferedCourse) => {
     section,
     course,
     faculty,
+    days,
+    startTime,
+    endTime,
   } = payload;
 
   //* check if the semester registration exists
@@ -93,9 +96,41 @@ const createOfferedCourse = async (payload: TOfferedCourse) => {
     );
   }
 
-  const result = await OfferedCourse.create({ ...payload, academicSemester });
+  //* validate time complexity for the faculty to take course
 
+  // get the schedules of the faculties
+  const assignedSchedules = await OfferedCourse.find({
+    semesterRegistration,
+    faculty,
+    days: { $in: days },
+  }).select('days startTime endTime');
+
+  //   console.log(assignedSchedules);
+
+  const newSchedule = {
+    days,
+    startTime,
+    endTime,
+  };
+
+  assignedSchedules.forEach((schedule) => {
+    const existingStartTime = new Date(`1970-01-01T${schedule.startTime}`);
+    const existingEndTime = new Date(`1970-01-01T${schedule.endTime}`);
+    const newStartTime = new Date(`1970-01-01T${newSchedule.startTime}`);
+    const newEndTime = new Date(`1970-01-01T${newSchedule.endTime}`);
+
+    if (newStartTime < existingEndTime && newEndTime > existingStartTime) {
+      throw new AppError(
+        httpStatus.BAD_REQUEST,
+        'This faculty is not available at that time! Choose other time or day.',
+      );
+    }
+  });
+
+  const result = await OfferedCourse.create({ ...payload, academicSemester });
   return result;
+
+  //   return null;
 };
 
 export const OfferedCourseServices = {
