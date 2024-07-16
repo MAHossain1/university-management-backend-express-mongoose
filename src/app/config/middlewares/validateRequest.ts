2639838aba3515b9951c6216c88b057be0998764
MAHossain1 <mahpro110@gmail.com>
2024-07-16 12:37:09 +0600

    Create auth middleware to verify accessToken.

--- src/app/config/middlewares/validateRequest.ts
+++ src/app/config/middlewares/validateRequest.ts
@@ -1,20 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
 import { AnyZodObject, ZodEffects } from 'zod';
+import catchAsync from '../../utils/catchAsync';
 
-const validateRequest =
-  (schema: AnyZodObject | ZodEffects<AnyZodObject>) =>
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await schema.parseAsync({
-        body: req.body,
-        query: req.query,
-        params: req.params,
-        cookies: req.cookies,
-      });
-      return next();
-    } catch (error) {
-      next(error);
-    }
-  };
+const validateRequest = (schema: AnyZodObject | ZodEffects<AnyZodObject>) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    await schema.parseAsync({
+      body: req.body,
+      query: req.query,
+      params: req.params,
+      cookies: req.cookies,
+    });
+
+    next();
+  });
+};
 
 export default validateRequest;
--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import validateRequest from '../../config/middlewares/validateRequest';
 import { AuthValidation } from './auth.validation';
 import { AuthControllers } from './auth.controller';
+import auth from '../../config/middlewares/auth';
 
 const router = express.Router();
 
 router.post(
   '/login',
+  auth(),
   validateRequest(AuthValidation.loginValidationSchema),
   AuthControllers.loginUser,
 );
