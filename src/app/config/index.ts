4e7eab1f4ea78159c6e03d03a23ee208944fe11f
MAHossain1 <mahpro110@gmail.com>
2024-07-18 12:28:56 +0600

    Refresh token set in cookies.

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -12,4 +12,7 @@ export default {
   default_student_password: process.env.DEFAULT_STUDENT_PASS,
   default_faculty_password: process.env.DEFAULT_FACULTY_PASS,
   jwt_access_token: process.env.JWT_ACCESS_TOKEN,
+  jwt_refresh_token: process.env.JWT_REFRESH_TOKEN,
+  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
+  jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
 };
--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -6,6 +6,7 @@ import sendResponse from '../../utils/sendResponse';
 import { AuthServices } from './auth.service';
 import { JwtPayload } from 'jsonwebtoken';
 import AppError from '../../errors/AppError';
+import config from '../../config';
 
 function isJwtPayload(user: any): user is JwtPayload {
   return user !== null && typeof user === 'object';
@@ -15,11 +16,21 @@ const loginUser = catchAsync(
   async (req: Request, res: Response): Promise<void> => {
     const result = await AuthServices.loginUser(req.body);
 
+    const { refreshToken, accessToken, needsPasswordChange } = result;
+
+    res.cookie('refreshToken', refreshToken, {
+      secure: config.node_env === 'production',
+      httpOnly: true,
+    });
+
     sendResponse(res, {
       success: true,
       statusCode: httpStatus.OK,
       message: 'User logged in successfully.',
-      data: result,
+      data: {
+        accessToken,
+        needsPasswordChange,
+      },
     });
   },
 );
