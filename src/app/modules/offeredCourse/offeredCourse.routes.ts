2413f5f5ad7aaeb0d7e874267eb663c6abc01231
MAHossain1 <mahpro110@gmail.com>
2024-08-10 12:21:41 +0600

    Added auth to offered Course.

--- src/app/modules/offeredCourse/offeredCourse.routes.ts
+++ src/app/modules/offeredCourse/offeredCourse.routes.ts
@@ -2,14 +2,26 @@ import express from 'express';
 import validateRequest from '../../config/middlewares/validateRequest';
 import { OfferedCourseValidationSchema } from './offeredCourse.validation';
 import { OfferedCourseControllers } from './offeredCourse.controller';
+import auth from '../../config/middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
-router.get('/', OfferedCourseControllers.getAllOfferedCourses);
+router.get(
+  '/',
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
+  OfferedCourseControllers.getAllOfferedCourses,
+);
 router.get('/:id', OfferedCourseControllers.getSingleOfferedCourse);
 
 router.post(
   '/create-offered-course',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin, USER_ROLE.student),
   validateRequest(
     OfferedCourseValidationSchema.createOfferedCourseValidationSchema,
   ),
@@ -18,12 +30,17 @@ router.post(
 
 router.patch(
   '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin, USER_ROLE.faculty),
   validateRequest(
     OfferedCourseValidationSchema.updateOfferedCourseValidationSchema,
   ),
   OfferedCourseControllers.updateOfferedCourse,
 );
 
-router.delete('/:id', OfferedCourseControllers.deleteOfferedCourse);
+router.delete(
+  '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin, USER_ROLE.faculty),
+  OfferedCourseControllers.deleteOfferedCourse,
+);
 
 export const OfferedCourseRoutes = router;
